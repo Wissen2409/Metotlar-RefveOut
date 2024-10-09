5a436383f72bf500b67b9a28e83a49703efd03d7
emrahelis <emrahelis@gmail.com>
2024-10-09 20:35:40 +0300

    sds

--- Program.cs
+++ Program.cs
@@ -54,21 +54,52 @@ Console.WriteLine("Out : "+a);
 
 // .net içerisindeki out kullanan bir metot var mı ?
 // int.TryParse
-
 int b;
 bool isOk = int.TryParse("100",out b);
 // bu metot ,bir string'i int'e çevirip çevirmeyeceğini kontrol eder, eğer çevirirse, out ile , yukarıdaki 
 // yukarıdaki başlangıç değeri olmayan b değişkenine dönüştürülmüş değeri atayacaktır
 // dönüştiremez ise, geri dönüş değeri olarak false dönecektir.
 // int tipinin başlangıç değeri 0'dir
-
 Console.WriteLine(isOk);
 Console.WriteLine(b);
-
-
 static void DegerAta(out int a)
 {
     // out ile gelen a değişkenine metot içerisindeki başlangıç değeri verildi.
     a=10;
 }
 #endregion
+
+// Kendi TryParse metodunuzu yazınız
+/* 
+1 Parametre      : Strng
+2 Parametre      : out int
+Geri dönüş tipi  : 
+*/
+int degisken;
+bool isResult = MyTryParse("10 10 " , out degisken);
+Console.WriteLine(isResult);
+Console.WriteLine(degisken);
+static bool MyTryParse(string value,out int b){
+
+    bool isLetter = false;
+    for(int i=0;i<value.Length;i++){
+
+        // string içerisinde boşluk karakteri olduğunda, tryparse hata verdi, boşluk karakterlerini 
+        // isWhiteSpace ile kontrol edebilir yada direk Trim yaptıktan sonra parse işlemi gerçekleştirebilirsiniz
+        //char.IsWhiteSpace
+        if(char.IsLetter(value[i])){
+
+            isLetter = true;
+            break;
+        }
+    }
+    
+    // ! operatörü ile isLetter False ise değerini kontrol etmiş oluyoruz
+    // b ye sıfır değeri verdik.
+    // if bloğuna girerse değerini alır girmezse, 0 olarak kalacak
+    b=0;
+    if(!isLetter){
+        b = int.Parse(value);
+    }
+    return !isLetter;
+}
--- obj/Debug/net8.0/Metotlar-RefveOut.AssemblyInfo.cs
+++ obj/Debug/net8.0/Metotlar-RefveOut.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Metotlar-RefveOut")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+833db8f69883ff6fbab1bae0d62043c52e4726d3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Metotlar-RefveOut")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Metotlar-RefveOut")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
